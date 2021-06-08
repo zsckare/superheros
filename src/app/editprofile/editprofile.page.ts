fc65221e97cb5544e6769f04b9dbf0d9df3925b7
Antonio Alvarez <tokyo.blue.tsubasa@gmail.com>
2021-06-08 15:12:44 -0500

    add edit

--- src/app/editprofile/editprofile.page.ts
+++ src/app/editprofile/editprofile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatabaseService} from '../services/database.service';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-editprofile',
   templateUrl: './editprofile.page.html',
@@ -7,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditprofilePage implements OnInit {
 
-  constructor() { }
-
+  data:any;
+  constructor(private route: ActivatedRoute, private router: Router, private dbService: DatabaseService,private navCtrl: NavController) {
+    console.log("CARGANDO")
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data = JSON.parse(this.router.getCurrentNavigation().extras.state.special);
+        
+        console.log(this.data)
+        console.log("CARGADO")
+      }
+    });
+  }
+  
   ngOnInit() {
   }
 
 
   save(){
-    
+    this.dbService.updateUser(this.data.id,this.data).then((res)=>{
+      this.navCtrl.pop()
+    })
   }
 }
